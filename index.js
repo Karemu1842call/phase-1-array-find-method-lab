c7b1acbe5834e209b98ce7be2ef1d263b34e58b6
Karemu1842call <victorkinoti03@gmail.com>
2022-06-14 09:20:37 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// code your solution here
+const record = [
+    { year: "2016", result: "W"},
+    { year: "2017", result: "L"},
+    { year: "2019", result: "L"},
+    { year: "2020", result: "W"},
+  ]
+superbowlWin = (record) => {
+    const result = record.find( record => record.result === "W" );
+    return !!result ? result.year : undefined;
+  }
